d46b9767df66277da76ccb7be06dc94a843c38a5
navjeet2512 <89721011+navjeet2512@users.noreply.github.com>
2021-09-02 17:13:25 +0530

    Add files via upload

---
+++ compute.h
@@ -0,0 +1,11 @@
+/* 
+    Add required header files and function prototypes here
+    upload this file to submitty
+*/
+
+#include "math.h"
+#include "complex.h"
+complex_t complex_sum(complex_t C1, complex_t C2);
+complex_t complex_difference(complex_t C1, complex_t C2);
+complex_t complex_multiply(complex_t C1, complex_t C2);
+complex_t complex_divide(complex_t C1, complex_t C2);
---
+++ resistance.h
@@ -0,0 +1,23 @@
+/**
+ * @file resistance.h
+ * @author Navjeet
+ * @brief Resistor Colour Coding
+ * @version 0.1
+ * @date 2021-09-02
+ * 
+ * @copyright Copyright (c) 2021
+ *
+ * 
+ */
+
+#ifndef RESISTANCE_H
+#define RESISTANCE_H
+
+#include "math.h"
+
+typedef struct resitance
+{
+    int first_colour, second_colour, third_colour, fourth_colour, fifth_colour, sixth_colour;
+}colour;
+
+#endif 
