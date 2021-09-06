6ce86e64969a3736f07a1900d4153250d655018b
navjeet2512 <mnavjeetsingh25@gmail.com>
2021-09-06 12:34:03 +0530

    Added Range

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -38,23 +38,23 @@ void test_tolerance()
 
 int main()
 {
-    UNITY_BEGIN();
-    RUN_TEST(test_resistance);
-    RUN_TEST(test_tolerance);
-    return UNITY_END();
+   // UNITY_BEGIN();
+    //RUN_TEST(test_resistance);
+   // RUN_TEST(test_tolerance);
+   // return UNITY_END();
 
-    printf("Welcome to Resitor Colour Coding Calculator");
-    printf("Select Your Colour Digits");
-    printf("0 : Black");
-    printf("1 : Brown");
-    printf("2 : Red");
-    printf("3 : Orange");
-    printf("4 : Yellow");
-    printf("5 : Green");
-    printf("6 : Blue");
-    printf("7 : Violet");
-    printf("8 : Grey");
-    printf("9 : White");
+    printf("Welcome to Resitor Colour Coding Calculator\n");
+    printf("Select Your Colour Digits\n");
+    printf("0 : Black\n");
+    printf("1 : Brown\n");
+    printf("2 : Red\n");
+    printf("3 : Orange\n");
+    printf("4 : Yellow\n");
+    printf("5 : Green\n");
+    printf("6 : Blue\n");
+    printf("7 : Violet\n");
+    printf("8 : Grey\n");
+    printf("9 : White\n");
 
     int c1, c2, c3,c4;
     scanf("%d",&c1);
@@ -70,7 +70,8 @@ int main()
     
     int result = resistance(c1,c2,c3);
     float tolerance_output = tolerance(c4);
-    printf("The total resistacne of the Resistor is %d Ohm, With %f Tolerance.",result, tolerance_output);
+    printf("The total resistacne of the Resistor is %d Ohm, With %0.2f Tolerance.\n",result, tolerance_output);
+    printf("Ranges from %0.4f to %0.4f", (result-((tolerance_output*result)/100)), (result+((tolerance_output*result)/100)));
 
     return 0;
 }
